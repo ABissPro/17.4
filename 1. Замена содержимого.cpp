5d0b9a8e2416a3208b1312847118de256b6d60cc
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-25 14:59:32 +0300

    Add files via upload

---
+++ 1. Замена содержимого.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void swapp(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	
+	swapp(&a, &b);
+	std::cout << a << " " << b;
+}
